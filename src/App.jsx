6bf112db913c13b5bcf0466c393f9e3067941d59
mimihokuro <keetasan7@gmail.com>
2024-08-30 01:17:57 +0900

    🎨 なくてもよい文言を撤去し期間を示す記号を追加

--- src/App.jsx
+++ src/App.jsx
@@ -165,20 +165,21 @@ const DateCalculator = () => {
       <h1 className="font-bold text-2xl text-center">休日計算ツール</h1>
       <div className="mt-4">
         <div className="mt-2">
-          <label>期間開始日：</label>
           <input
             className="w-140 rounded-md border-0 py-1 pl-2 pr-2 text-left text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             type="date"
             value={startDate}
+            aria-labelledby="期間開始日"
             onChange={(e) => setStartDate(e.target.value)}
           />
         </div>
+        ＞
         <div className="mt-2">
-          <label>期間終了日：</label>
           <input
             className="w-140 rounded-md border-0 py-1 pl-2 pr-2 text-left text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             type="date"
             value={endDate}
+            aria-labelledby="期間終了日"
             onChange={(e) => setEndDate(e.target.value)}
           />
         </div>
