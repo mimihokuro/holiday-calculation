9bf75b5f205b8e2fa0007091edb3d16c8b9bb961
mimihokuro <keetasan7@gmail.com>
2024-08-31 15:14:10 +0900

    ♻️ オプション部分をコンポーネント化

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import SelectDate from "./components/SelectDate";
+import SelectOptions from "./components/SelectOptions";
 
 const DateCalculator = () => {
   const isHoliday = async (date) => {
@@ -23,7 +24,6 @@ const DateCalculator = () => {
 
   const [startDate, setStartDate] = useState(`${today.getFullYear()}-01-01`);
   const [endDate, setEndDate] = useState(`${today.getFullYear()}-12-31`);
-  const dateData = { startDate, setStartDate, endDate, setEndDate };
   const [option, setOption] = useState("sundays");
   const [between, setBetween] = useState(0);
   const [days, setDays] = useState(0);
@@ -161,48 +161,19 @@ const DateCalculator = () => {
     setDays(count);
   };
 
+  const dateData = { startDate, setStartDate, endDate, setEndDate };
+  const optionData = {
+    option,
+    businessHolidays,
+    optionHolidays,
+    handleOptionChange,
+  };
+
   return (
     <div className="bg-white mx-auto max-w-md py-24 sm:px-6 sm:py-32 lg:px-8">
       <h1 className="font-bold text-2xl text-center">休日計算ツール</h1>
       <SelectDate dateData={dateData} />
-      <table className="mt-2">
-        <tbody>
-          <tr>
-            <th className="w-32 text-left">集計する休日は</th>
-            <td className="flex gap-2 flex-wrap py-1 pl-2 pr-2">
-              {optionHolidays.map((oh) => {
-                return (
-                  <label key={oh.value}>
-                    <input
-                      type="radio"
-                      value={oh.value}
-                      checked={option === oh.value}
-                      onChange={handleOptionChange}
-                    />
-                    {oh.title}
-                  </label>
-                );
-              })}
-            </td>
-          </tr>
-          {businessHolidays.map((bh) => {
-            return (
-              <tr key={bh.title}>
-                <th className="w-32 text-left">{bh.title}</th>
-                <td className="py-1">
-                  <input
-                    className="w-14 rounded-md border-0 py-1 pl-2 pr-2 text-center text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    type="number"
-                    value={bh.value}
-                    onChange={bh.doing}
-                  />
-                  日
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <SelectOptions optionData={optionData} />
       <div className="flex gap-4 justify-center mt-4">
         <button
           className="rounded-md px-3.5 py-2.5 text-sm font-semibold text-gray-200 bg-green-600 shadow-sm hover:bg-green-500 active:bg-green-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
--- src/components/SelectOptions.jsx
+++ src/components/SelectOptions.jsx
@@ -1,19 +1,46 @@
-const SelectOptions = ({ options, handleOption }) => {
+const SelectOptions = ({ optionData }) => {
+  const { option, businessHolidays, optionHolidays, handleOptionChange } =
+    optionData;
+
   return (
-    <div>
-      {options.map((option) => {
-        return (
-          <label key={option.id}>
-            <input
-              type="radio"
-              name="holidayOptions"
-              onClick={() => handleOption(option.id)}
-            />
-            {option.content}
-          </label>
-        );
-      })}
-    </div>
+    <table className="mt-2">
+      <tbody>
+        <tr>
+          <th className="w-32 text-left">集計する休日は</th>
+          <td className="flex gap-2 flex-wrap py-1 pl-2 pr-2">
+            {optionHolidays.map((oh) => {
+              return (
+                <label key={oh.value}>
+                  <input
+                    type="radio"
+                    value={oh.value}
+                    checked={option === oh.value}
+                    onChange={handleOptionChange}
+                  />
+                  {oh.title}
+                </label>
+              );
+            })}
+          </td>
+        </tr>
+        {businessHolidays.map((bh) => {
+          return (
+            <tr key={bh.title}>
+              <th className="w-32 text-left">{bh.title}</th>
+              <td className="py-1">
+                <input
+                  className="w-14 rounded-md border-0 py-1 pl-2 pr-2 text-center text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  type="number"
+                  value={bh.value}
+                  onChange={bh.doing}
+                />
+                日
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 };
 
