141ff7958e985a3ca83e30b94c642301a3073b2c
mimihokuro <keetasan7@gmail.com>
2024-09-14 00:07:10 +0900

    🩹 日付選択部分のレイアウトがスマホで縦並びになっていなかったため修正

--- src/components/SelectDate.jsx
+++ src/components/SelectDate.jsx
@@ -1,11 +1,11 @@
 import { ArrowRightIcon } from "@chakra-ui/icons";
-import { HStack, Input, Text } from "@chakra-ui/react";
+import { Flex, Input, Text } from "@chakra-ui/react";
 
 const SelectDate = ({ dateData }) => {
   const { startDate, setStartDate, endDate, setEndDate } = dateData;
   return (
     <>
-      <HStack
+      <Flex
         placeItems="center"
         gap={4}
         direction={{ base: "column", sm: "row" }}
@@ -31,7 +31,7 @@ const SelectDate = ({ dateData }) => {
           aria-labelledby="期間終了日"
           onChange={(e) => setEndDate(e.target.value)}
         />
-      </HStack>
+      </Flex>
       {startDate > endDate ? (
         <Text color="palevioletred" fontWeight="bold" textAlign="center" mt={2}>
           正しい期間を選択してください
